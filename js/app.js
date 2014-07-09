94cdb21a0c3965c22d397cded0f9aa9df3016b7d
pwoloszun <piotr.woloszun@gmail.com>
2014-07-09 15:40:06 +0200

    v 1.0.5

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 App.init = function() {
-  // create user instance with params: {name: "Batman", age: 55, id: 99}
+  // create user instance with params: {name: "Batman", age: 55, id: 99, url: "http://rocketmind.pl"}
 
   // create view instance
 
