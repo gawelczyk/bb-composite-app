80ed7b58205ea79992ade3915bf81c0d483e81f8
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-17 10:28:07 +0200

    knockout in action

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-App.init = function() {
-  // create user instance with params: {name: "Batman", age: 55, id: 99, url: "http://rocketmind.pl"}
+App.init = function () {
+    // create user instance with params: {name: "Batman", age: 55, id: 99, url: "http://rocketmind.pl"}
 
-  // create view instance
+    var bob = new App.Models.User({name: "Batman", age: 55, id: 99, url: "http://rocketmind.pl"});
 
-  // render view
+    // create view instance
+    var userView = new App.Views.UserView({
+        model: bob
+    });
 
-  // print rendered view HTML to $(".js-app")
+    // render view
+    userView.render();
+
+    // print rendered view HTML to $(".js-app")
+    $('.js-app').append(userView.$el);
 
 };
 
-$(function() {
-  App.init();
+$(function () {
+    App.init();
 });
--- js/views/UserView.js
+++ js/views/UserView.js
@@ -1,2 +1,11 @@
 App.Views.UserView = Backbone.View.extend({
+
+    render: function () {
+        this.$el.html("<h3>Name: " + this.model.get('name') + "</h3>"
+            + "<p>url: <a href='" + this.model.get('url') + "'>" + this.model.get('url') + "</a> "
+            + "Age: " + this.model.get('age')
+            + "</p>");
+        return this;
+    }
+
 });
